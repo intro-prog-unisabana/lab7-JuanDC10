8d486847ca9ccd62c18241a6fefd27c1db45f281
JuanDC10 <juancastpar@unisabana.edu.co>
2026-04-09 16:01:08 -0500

    1.2

--- password_manager.py
+++ password_manager.py
@@ -6,12 +6,13 @@ from caesar import caesar_encrypt
 def encrypt_single_pass(filename: str) -> None:
     """TODO: Parte 1."""
     with open(filename, "r") as archivo: 
-        línea = archivo.read()
-        separar = línea.strip()
-    call = caesar_encrypt(separar)
-    with open(filename, "w") as document: 
+        password = archivo.read().strip()
+    call = caesar_encrypt(password)
+    with open(filename, "w") as documento: 
         documento.write(call)
-    return documento
+
+
+
 def encrypt_passwords_in_file(filename: str) -> None:
     """TODO: Parte 2."""
     pass
