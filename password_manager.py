41f07116fdb3113376389bb8e42adf09d6ddf045
JuanDC10 <juancastpar@unisabana.edu.co>
2026-04-16 18:37:10 -0500

    1.5

--- password_manager.py
+++ password_manager.py
@@ -22,16 +22,18 @@ def encrypt_passwords_in_file(filename: str) -> None:
         if i != 0:
             password = filas[i][2]
             filas[i][2] = caesar_encrypt(password)
-
-
-
-
-
+    with open(filename, "w", newline="") as archivo:
+        escritor = csv.writer(archivo)
+        for fila in filas:
+            escritor.writerow(fila)
 
 def change_password(filename: str, website: str, password: str) -> bool:
-    """TODO: Parte 3."""
-    pass
-
+    filas = []
+    with open(filename, "r") as archivo:
+        lector = csv.reader(archivo)
+        for fila in lector:
+            if fila:
+                filas.append(fila)
 
 def add_login(filename: str, website_name: str, username: str, password: str) -> None:
     """TODO: Parte 4."""
