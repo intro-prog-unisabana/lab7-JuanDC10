851a1e7f5d602b4b3a110094325a7bed58a2baf0
JuanDC10 <juancastpar@unisabana.edu.co>
2026-04-16 20:17:49 -0500

    2.1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from password_manager import add_login, change_password, encrypt_passwords_in_file
 
-
 def main() -> None:
-    """TODO: Parte 5 - programa principal interactivo."""
-    pass
+    print("Enter the CSV file name:")
+    filename = input()
+
 
 
 if __name__ == "__main__":
